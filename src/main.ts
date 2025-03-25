4a58c5bad1db41daa45aee82fd5e7cdfb76e9013
y4edd <yaeyuu0116@icloud.com>
2025-03-25 15:48:19 +0900

    fix: originを許可するサイトのURL修正

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
     credentials: true,
-    origin: [
-      'http://localhost:3000',
-      'https://frontend-todo-nextjs.vercel.app',
-    ],
+    origin: ['http://localhost:3000', 'https://nextjs-todo-eight.vercel.app/'],
   });
   app.use(cookieParser());
   app.use(
